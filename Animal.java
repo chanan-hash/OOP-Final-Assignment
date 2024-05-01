ce3d972720a223ea5cf33e78d4d76dc73f2507e8
chanan-hash <67066282+chanan-hash@users.noreply.github.com>
2024-05-01 21:12:46 +0300

    Adding the final assignment

---
+++ Animal.java
@@ -0,0 +1,10 @@
+/**
+ * Question 3
+ */
+public class Animal {
+    final static String sound = "Animal sound";
+
+    static void makeSound() {
+        System.out.println(sound);
+    }
+}
---
+++ Dog.java
@@ -0,0 +1,7 @@
+public class Dog extends Animal {
+        final static String sound = "Bark";
+
+        static void makeSound() {
+        System.out.println(sound);
+    }
+}
